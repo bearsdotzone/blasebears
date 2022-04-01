cf90305a72931cae3eb0911bd7f18ce67183ef51
bears <bears@bears.zone>
2022-04-01 16:49:09 -0500

    Stylized header

--- components/common/Header/header.js
+++ components/common/Header/header.js
@@ -1,9 +1,22 @@
 import styles from "../../../styles/Home.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import baseball from "../../../images/baseball.gif";
 
 function HeaderComponent() {
   return (
     <div className={styles.header}>
-      <h1>Internet League Blaseball Fans Web Ring</h1>
+      <div className={styles.rowcontainer}>
+        <Image src={baseball} layout="fixed" />
+        <h1>Internet League Blaseball Fans Web Ring</h1>
+        <Image src={baseball} layout="fixed" />
+      </div>
+      <Link href="/">
+        <a>Home</a>
+      </Link>{" "}
+      <Link href="/resources">
+        <a>Resources</a>
+      </Link>
     </div>
   );
 }
