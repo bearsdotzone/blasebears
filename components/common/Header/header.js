98ec738cf7326d057f927de06434db7a82d19a3b
bears <bears@bears.zone>
2022-04-02 09:42:12 -0500

    Added discovery links to header

--- components/common/Header/header.js
+++ components/common/Header/header.js
@@ -1,22 +1,37 @@
-import styles from "../../../styles/Home.module.css";
+import styles from "./HeaderStyles.module.css";
 import Link from "next/link";
 import Image from "next/image";
-import baseball from "../../../images/baseball.gif";
+import prev from "../../../images/prev.gif";
+import next from "../../../images/next.gif";
 
 function HeaderComponent() {
   return (
     <div className={styles.header}>
       <div className={styles.rowcontainer}>
-        <Image src={baseball} layout="fixed" unoptimized="true" />
+        <Link href="https://ring.yell.rocks/prev">
+          <Image src={prev} layout="fixed" unoptimized="true" />
+        </Link>
         <h1>Internet League Blaseball Fans Web Ring</h1>
-        <Image src={baseball} layout="fixed" unoptimized="true" />
+        <Link href="https://ring.yell.rocks/next">
+          <Image src={next} layout="fixed" unoptimized="true" />
+        </Link>
       </div>
-      <Link href="/">
-        <a>Home</a>
-      </Link>{" "}
-      <Link href="/resources">
-        <a>Resources</a>
-      </Link>
+      <div className={styles.rowcontainer}>
+        <Link href="https://ring.yell.rocks/prev">
+          <a>Previous</a>
+        </Link>
+        <span>
+          <Link href="/">
+            <a>Home</a>
+          </Link>{" "}
+          <Link href="/resources">
+            <a>Resources</a>
+          </Link>
+        </span>
+        <Link href="https://ring.yell.rocks/next">
+          <a>Next</a>
+        </Link>
+      </div>
     </div>
   );
 }
