3b211e2dffe7a152e434743e4f5c5336213fc58b
bears <bears@bears.zone>
2022-04-02 09:42:45 -0500

    Pulled discovery links from footer, added under construction gif

--- components/common/Footer/footer.js
+++ components/common/Footer/footer.js
@@ -1,29 +1,27 @@
-import styles from "../../../styles/Home.module.css";
+import styles from "./FooterStyles.module.css";
+import construction from "../../../images/construction.gif";
+import Image from "next/image";
 
 function FooterComponent() {
   return (
     <div className={styles.footer}>
-      github:{" "}
-      <a href="https://github.com/bearsdotzone/blasebears">
-        this website is open source software
-      </a>{" "}
-      distributed under the{" "}
-      <a href="https://anticapitalist.software/">
-        anticapitalist software license
-      </a>{" "}
-      twitter:{" "}
-      <a href="https://github.com/bearsdotzone/blasebears">@bearsdotzone</a>{" "}
-      <br />
-      Part of the{" "}
-      <a href="https://ring.yell.rocks/">
-        Internet League Blaseball Fans Web Ring
-      </a>
-      <br />
-      <a href="https://ring.yell.rocks/prev">Previous Website</a>{" "}
-      <a href="https://ring.yell.rocks/random">Random Website</a>{" "}
-      <a href="https://ring.yell.rocks/next">Next Website</a>
-      <br />
-      Bears loves Alexandra 💖
+      <div className={styles.footercontainer}>
+        <p>
+          github:{" "}
+          <a href="https://github.com/bearsdotzone/blasebears">
+            this website is open source software
+          </a>{" "}
+          distributed under the{" "}
+          <a href="https://anticapitalist.software/">
+            anticapitalist software license
+          </a>{" "}
+          twitter:{" "}
+          <a href="https://github.com/bearsdotzone/blasebears">@bearsdotzone</a>{" "}
+          <br />
+          Bears loves Alexandra 💖
+        </p>
+        <Image src={construction} />
+      </div>
     </div>
   );
 }
